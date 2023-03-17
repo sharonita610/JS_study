e6672010bdef1dfeb744d10d8ae031fb7c4ffe97
Sharon Kim <sharonita610@gmail.com>
2023-03-17 12:42:54 +0900

    js_study 3.17오전수업, 2.배열 3.quiz my code

---
+++ 1.자바스크립트 기초/17.암묵적 타입변환.js
@@ -0,0 +1,44 @@
+
+
+var n1 = 10;
+var n2 = '20';
+
+console.log(n1+n2);
+
+var n3 = n1 + '';
+console.log(typeof n3);
+
+var n4 = '100' - 55;
+console.log(n4);
+
+console.log(`=====================================`);
+
+// js 에서는 0, null, undefined, NaN는 거짓으로 판단한다
+
+
+// Falsy
+if (undefined) console.log('ok1');
+if (null) console.log('ok2');
+if (0) console.log('ok3');
+if ("") console.log('ok4');
+if (NaN) console.log('ok5');
+
+// Truthy
+if (1) console.log('ok6');
+if (-100.55) console.log('ok7');
+if ('   ') console.log('ok8');
+if ('hello!!!!') console.log('ok9');
+if ([]) console.log('ok10');
+
+for (var  n = 1; n <= 10; n++){
+    if(n % 2 === 0){
+        console.log(n + `:짝수입니다.`);
+    } else {
+        console.log(n + `:홀수입니다.`);
+
+    }
+}
+
+// while(999){
+//      or '메롱' 도 true 라서 무한 루프가 가능하다
+// }
---
+++ 1.자바스크립트 기초/19.단축평가.js
@@ -0,0 +1,22 @@
+
+
+// OR 연산 : 첫번째 truthy를 반환
+
+console.log('hello' || 'world');
+console.log(0 || '안녕');
+
+// AND 연산 : 첫번째 falsy 를 반환
+console.log( `================`);
+
+console.log('hello' && 'WORLD');
+console.log(null && '메롱');
+
+/*
+    <h1>안녕하세요 우리 홈페이지에 오신걸 환영합니다 </h1>
+    isLogin()  && <h2>xxx 님 안녕하세요! </h2>
+
+    쿠폰당첨 플래그 && sendCoupon()
+
+    isMine && <button>수정</button>
+
+ */
